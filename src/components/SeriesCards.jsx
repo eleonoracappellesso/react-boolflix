c89b9d9e7792a97f006ef817939baaaf9d0220e0
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-01-14 18:48:46 +0100

    created overlay effect to series cards

--- src/components/SeriesCards.jsx
+++ src/components/SeriesCards.jsx
@@ -1,8 +1,31 @@
 import { useContext } from 'react';
 import { GlobalContext } from '../contexts/GlobalContext';
 
+import style from "./Card.module.css";
+
+const flags = ["de", "en", "es", "it", "fr"];
+
 export default function SeriesCards() {
     const { series, loading } = useContext(GlobalContext); // Ottieni series dal contesto
+
+    const flag = flags.includes(series.original_language)
+        ? series.original_language + ".jpg"
+        : "placeholder.png";
+
+    const drawStars = () => {
+        let stars = [];
+        for (let i = 1; i <= 5; i++) {
+            const star =
+                i <= Math.ceil(series.vote_average / 2) ? (
+                    <i className='fa-star' key={i} />
+                ) : (
+                    <i className='fa-reg-star' key={i} />
+                );
+            stars.push(star);
+        }
+        return stars;
+    };
+
 
     return (
         <div className="row">
@@ -11,17 +34,22 @@ export default function SeriesCards() {
             ) : series.length > 0 ? (
                 series.map((serie) => (
                     <div key={serie.id} className="col-md-3 mb-4">
-                        <div className="card">
+                        <div className={`card ${style.cardWrapper} ${style.cardEffect}`}>
                             {serie.poster_path && (
                                 <img
                                     src={`https://image.tmdb.org/t/p/w500${serie.poster_path}`}
                                     alt={serie.name}
-                                    className="card-img-top"
+                                    className={`card-img-top ${style.cardImg}`}
                                 />
                             )}
                         </div>
-                        <div className="card-body">
-                            <h5>{serie.name}</h5>
+                        <div className={style.cardInner}>
+                            <h5>{serie.title}</h5>
+                            <p className='p-2'>{serie.overview}</p>
+                            <div className={style.flag}>
+                                <img src={`/img/${flag}`} alt={flag} className="img-fluid" />
+                            </div>
+                            <div className={style.cardStar}>{drawStars()}</div>
                         </div>
                     </div>
                 ))
