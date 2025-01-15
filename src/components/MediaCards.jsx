24396da4252b16dd7d916331a9616d96c42faf83
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-01-15 15:57:27 +0100

    fixing style

--- src/components/MediaCards.jsx
+++ src/components/MediaCards.jsx
@@ -1,53 +1,3 @@
-// import { useContext } from 'react';
-// import { GlobalContext } from '../contexts/GlobalContext';
-
-// // import carousel component from react-slick
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css"
-
-// import Card from './Card';
-
-// const flags = ["de", "en", "es", "it", "fr"];
-
-// export default function MediaCards({ dataKey, loadingMessage, emptyMessage }) {
-//     const { loading, ...dataSets } = useContext(GlobalContext);
-//     const data = dataSets[dataKey] || [];
-
-//     const flag = (language) =>
-//         flags.includes(language) ? language + ".jpg" : "placeholder.png";
-
-//     const carouselSettings = {
-//         dots: true,
-//         infinite: true,
-//         speed: 500,
-//         slidesToShow: 4,
-//         slidesToScroll: 1
-//     }
-
-//     return (
-//         <div className="row">
-//             <Slider {...carouselSettings}>
-//                 {loading ? (
-//                     <p>{loadingMessage}</p>
-//                 ) : data.length > 0 ? (
-//                     data.map((item) => (
-//                         <div key={item.id} className="col-md-3 mb-4">
-//                             <Card
-//                                 item={item}
-//                                 flag={flag(item.original_language)}
-//                             />
-//                         </div>
-//                     ))
-//                 ) : (
-//                     <p>{emptyMessage}</p>
-//                 )
-//                 }
-//             </Slider>
-//         </div>
-//     );
-// }
-
 import { useContext } from "react";
 import { GlobalContext } from "../contexts/GlobalContext";
 import Slider from "react-slick";
@@ -61,7 +11,6 @@ const flags = ["de", "en", "es", "it", "fr"];
 export default function MediaCards({ dataKey, loadingMessage, emptyMessage }) {
     const { loading, ...dataSets } = useContext(GlobalContext);
 
-    // Ottieni il set di dati specifico in base a dataKey
     const data = dataSets[dataKey] || [];
 
     const flag = (language) =>
@@ -95,7 +44,7 @@ export default function MediaCards({ dataKey, loadingMessage, emptyMessage }) {
         ],
     };
 
-    // Mostra un messaggio di caricamento o di assenza di dati, se necessario
+    // Mostra un messaggio di caricamento o di assenza di dati
     if (loading) {
         return <p>{loadingMessage}</p>;
     }
