29ba206eff1bf3692ff5739656e16735df975981
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-01-15 15:34:47 +0100

    added carousel in media cards with react slick

--- src/components/MediaCards.jsx
+++ src/components/MediaCards.jsx
@@ -1,5 +1,11 @@
 import { useContext } from 'react';
 import { GlobalContext } from '../contexts/GlobalContext';
+
+// import carousel component from react-slick
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css"
+
 import Card from './Card';
 
 const flags = ["de", "en", "es", "it", "fr"];
@@ -11,22 +17,33 @@ export default function MediaCards({ dataKey, loadingMessage, emptyMessage }) {
     const flag = (language) =>
         flags.includes(language) ? language + ".jpg" : "placeholder.png";
 
+    const carouselSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    }
+
     return (
         <div className="row">
-            {loading ? (
-                <p>{loadingMessage}</p>
-            ) : data.length > 0 ? (
-                data.map((item) => (
-                    <div key={item.id} className="col-md-3 mb-4">
-                        <Card
-                            item={item}
-                            flag={flag(item.original_language)}
-                        />
-                    </div>
-                ))
-            ) : (
-                <p>{emptyMessage}</p>
-            )}
+            <Slider {...carouselSettings}>
+                {loading ? (
+                    <p>{loadingMessage}</p>
+                ) : data.length > 0 ? (
+                    data.map((item) => (
+                        <div key={item.id} className="col-md-3 mb-4">
+                            <Card
+                                item={item}
+                                flag={flag(item.original_language)}
+                            />
+                        </div>
+                    ))
+                ) : (
+                    <p>{emptyMessage}</p>
+                )
+                }
+            </Slider>
         </div>
     );
 }
